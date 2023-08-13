7904d875f2cad53f387aaec78b8d7d9cff5a1952
Alessio Caputo <84250128+alessioC42@users.noreply.github.com>
2023-08-13 12:20:51 +0200

    progress

--- src/api.js
+++ src/api.js
@@ -2,22 +2,39 @@ const express = require("express");
 const betterSQLite3 = require("better-sqlite3");
 
 const api = express.Router();
-const db = betterSQLite3(__dirname + "/database.db", { "fileMustExist": true });
+const db = betterSQLite3(__dirname + "/database.db", { "fileMustExist": true, "verbose": console.log});
 
-const dbQuerrys = {
+const dbQuerys = {
     getAllMembers:          db.prepare("SELECT * FROM members;"),
-    getSingeMemberByID:     db.prepare("SELECT * FROM members WHERE id= ?;"),
-    addSingleMember:        db.prepare("INSERT INTO members (first_name, second_name, gender, birthday, address, email, phone, phone2, function_level, team, family) VALUES ($first_name, $second_name, $gender, $birthday, $address, $email, $phone, $phone2, $function_level, $team, $family);"),
+    getSingeMemberByID:     db.prepare("SELECT * FROM members WHERE id=?;"),
+    addSingleMember:        db.prepare("INSERT INTO members (first_name, second_name, gender, birthday, address, email, phone, phone2, function_level, team, family, active, debit) VALUES ($first_name, $second_name, $gender, $birthday, $address, $email, $phone, $phone2, $function_level, $team, $family, $active, $debit);"),
     getAllFamilys:          db.prepare("SELECT * FROM familys"),
-    getSingeFamilyByName:   db.prepare("SELECT * FROM familys WHERE familyname= ?;"),
-    addSingleFamily:        db.prepare("INSERT INTO familys (familyname, parent_first_name, parent_second_name, parent_gender, parent_address, parent_email, parent_phone, parent_phone2, generated) VALUES ($familyname, $parent_first_name, $parent_second_name, $parent_gender, $parent_address, $parent_email, $parent_phone, $parent_phone2, $generated);")
+    getAllFamilyNames:      db.prepare("SELECT familyname FROM familys"),
+    getAllFamilyMembers:    db.prepare("SELECT id, first_name, second_name FROM members WHERE id=?;"),
+    deleteMember:           db.prepare("DELETE FROM members WHERE id=?;"),
+    getSingeFamilyByName:   db.prepare("SELECT * FROM familys WHERE familyname=?;"),
+    addSingleFamily:        db.prepare("INSERT INTO familys (familyname, parent_first_name, parent_second_name, parent_gender, parent_address, parent_email, parent_phone, parent_phone2, generated) VALUES ($familyname, $parent_first_name, $parent_second_name, $parent_gender, $parent_address, $parent_email, $parent_phone, $parent_phone2, $generated);"),
+    addGeneratedFamily:     db.prepare("INSERT INTO familys (familyname, generated) VALUES (?, 1);"),
+    deleteFamily:           db.prepare("DELETE FROM familys WHERE familyname=?;"),
+    getAllTeams:            db.prepare("SELECT * FROM teams;"),
+    getAllTeamNames:        db.prepare("SELECT teamname FROM teams;"),
+    getSingleTeam:          db.prepare("SELECT * FROM teams WHERE teamname=?;"),
+    getAllTeamMembers:      db.prepare("SELECT * FROM members WHERE team=?;"),
+    addSingleTeam:          db.prepare("INSERT INTO teams (teamname, leader1, leader2, leader3, notes) VALUES ($teamname, $leader1, $leader2, $leader3, $notes)"),
+    removeTeamConnections:  db.prepare("UPDATE members SET team='' WHERE team=?;"),
+    deleteTeam:             db.prepare("DELETE FROM teams WHERE teamname=?;")
 }
 
 /**
  * MEMBERS
  */
 api.get("/members", (_req, res) => {
-    let data = dbQuerrys.getAllMembers.all();
+    let data = dbQuerys.getAllMembers.all();
+
+    data.forEach((row, i) => {
+        data[i].age = getAge(row.birthday)
+    })
+
     res.json(data);
     res.end();
 });
@@ -25,7 +42,7 @@ api.get("/members", (_req, res) => {
 api.get("/member", (req, res) => {
     try {
         let id = (req.query.id);
-        let data = dbQuerrys.getSingeMemberByID.get(id);
+        let data = dbQuerys.getSingeMemberByID.get(id);
         res.json(data);
         res.end();
     } catch (err) {
@@ -36,9 +53,17 @@ api.get("/member", (req, res) => {
 
 api.post("/member/create", (req, res) => {
     try {
-        let data = JSON.parse(req.querry.data);
+        let data = JSON.parse(req.query.data);
 
-        dbQuerrys.addSingleMember.run(data);
+        if (data.team == "") {
+            data.team == null;
+        }
+
+        if (!data.family || data.family== "") {
+            res.status(500).end();
+        } else {
+            dbQuerys.addSingleMember.run(data);
+        }
     } catch (err) {
         console.log(err);
         res.status(500).end();
@@ -47,7 +72,6 @@ api.post("/member/create", (req, res) => {
 
 api.post("/member/update", (req, res) => {
     try {
-        console.log(req.query)
         let data = JSON.parse(req.query.data);
         let id = Number(req.query.id);
 
@@ -63,36 +87,73 @@ api.post("/member/update", (req, res) => {
     }
 });
 
+api.delete("/member/delete", (req, res) => {
+    //TODO member in tempteam
+    try {
+        let id = JSON.parse(req.query.id);
+        dbQuerys.deleteMember.run(id);
+    } catch (err) {
+        console.log(err);
+        res.status(500).end();
+    }
+});
+
 /**
- * FAMILY
+ * FAMILYS
  */
-api.get("/familys", (req, res) => {
-    let data = dbQuerrys.getAllFamilys.all();
-    res.json(data);
-    res.end();
+api.get("/familys", (_req, res) => {
+    let data = dbQuerys.getAllFamilys.all();
+    res.json(data).status(200).end();
+});
+
+api.get("/familys/list", (_req, res) => {
+    let data = dbQuerys.getAllFamilyNames.all();
+    let familys = []
+    data.forEach(elem => {
+        familys.push(elem.familyname)
+    });
+    res.json(familys).status(200).end();
 })
 
 api.get("/family", (req, res) => {
     try {
         let familyname = (req.query.familyname);
-        let data = dbQuerrys.getSingeMemberByID.get(familyname);
-        res.json(data);
-        res.end();
+        let data = dbQuerys.getSingeFamilyByName.get(familyname);
+        res.json(data).status(200).end();
     } catch (err) {
         console.log(err)
         res.status(500).end();
     }
 });
 
+api.get("/family/members", (req, res) => {
+    let familyname = req.query.familyname;
+    let data = dbQuerys.getAllFamilyMembers.all(familyname);
+    res.json(data).status(200).end();
+});
+
 api.post("/family/create", (req, res) => {
     try {
-        let data = JSON.parse(req.querry.data);
+        let data = JSON.parse(req.query.data);
 
         if (!data.generated) {
             data.generated = 0;
         }
 
-        dbQuerrys.addSingleFamily.run(data);
+        dbQuerys.addSingleFamily.run(data);
+        res.status(200).end();
+    } catch (err) {
+        console.log(err);
+        res.status(500).end();
+    }
+});
+
+api.post("/family/create/generate", (req, res) => {
+    try {
+        let familyname = (req.query.familyname);
+
+        dbQuerys.addGeneratedFamily.run(familyname);
+        res.status(200).end();
     } catch (err) {
         console.log(err);
         res.status(500).end();
@@ -116,9 +177,117 @@ api.post("/family/update", (req, res) => {
     }
 });
 
+api.delete("/family/delete", (req, res) => {
+    try {
+        let familyname = req.query.familyname;
+
+        let members = dbQuerys.getAllFamilyMembers.all(familyname);
+
+        if (members.length >=1) {
+            res.status(400).send(members).end();
+        } else {
+            dbQuerys.deleteFamily.run(familyname);
+            res.status(200).end();
+        }
+        
+    } catch (err) {
+        console.log(err);
+        res.status(500).end();
+    }
+});
+
+/**
+ *  TEAMS
+ */
+api.get("/teams", (_req, res) => {
+    let data = dbQuerys.getAllTeams.all();
+    res.json(data).status(200).end();
+});
+
+api.get("/teams/list", (_req, res) => {
+    let data = dbQuerys.getAllTeamNames.all();
+    let teams = []
+    data.forEach(elem => {
+        teams.push(elem.teamname)
+    });
+    res.json(teams).status(200).end();
+});
+
+api.get("/team", (req, res) => {
+    try {
+        let teamname = (req.query.teamname);
+        let data = dbQuerys.getSingleTeam.get(teamname);
+        res.json(data).status(200).end();
+    } catch (err) {
+        console.log(err)
+        res.status(500).end();
+    }
+});
+
+api.get("/team/members", (req, res) => {
+    let teamname = req.query.teamname;
+    let data = dbQuerys.getAllTeamMembers.all(teamname);
+    res.json(data).status(200).end();
+});
+
+api.post("/team/create", (req, res) => {
+    try {
+        let data = JSON.parse(req.query.data);
+        dbQuerys.addSingleTeam.run(data);
+        res.status(200).end();
+    } catch (err) {
+        console.log(err);
+        res.status(500).end();
+    }
+});
+
+api.post("/team/update", (req, res) => {
+    try {
+        console.log(req.query);
+        let data = JSON.parse(req.query.data);
+        let teamname = Number(req.query.teamname);
+
+        Object.keys(data).forEach((column, _index) => {
+            db.prepare(`UPDATE team SET ${column}=${escapeSQL(data[column])} WHERE teamname=${teamname};`).run();
+        });
+        
+        res.status(200).end();
+    } catch (err) {
+        console.log(err);
+        res.status(500).end();
+    }
+});
+
+api.delete("/team/delete", (req, res) => {
+    try {
+        let teamname = req.query.teamname;
+
+        dbQuerys.removeTeamConnections.run(teamname);
+        dbQuerys.deleteTeam.run(teamname);
+        res.status(200).end();
+    } catch (err) {
+        console.log(err);
+        res.status(500).end();
+    }
+});
+
+/**
+ *  FUNCTIONS
+ */
 function escapeSQL(value) {
+    if (value == null) {return "NULL"}
     return "'" + String(value).replace(/'/g, "''") + "'";
 }
 
+function getAge(dateString) {
+    var today = new Date();
+    var birthDate = new Date(dateString);
+    var age = today.getFullYear() - birthDate.getFullYear();
+    var m = today.getMonth() - birthDate.getMonth();
+    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+        age--;
+    }
+    return age;
+}
 
 module.exports = api;
--- src/index.js
+++ src/index.js
@@ -7,7 +7,10 @@ const app = express();
 app.use("/", express.static(__dirname + "/static"));
 app.use("/bootstrap", express.static(__dirname + "/node_modules/bootstrap/dist"));
 app.use("/jquery", express.static(__dirname + "/node_modules/jquery/dist"));
-app.use("/api", api)
+app.use("/bootstrap-table", express.static(__dirname + "/node_modules/bootstrap-table/dist"));
+app.use("/bootstrap-icons", express.static(__dirname + "/node_modules/bootstrap-icons"));
+app.use("/gridjs", express.static(__dirname + "/node_modules/gridjs/dist/"))
+app.use("/api", api);
 
 app.listen(3001, () => {
     console.log("running...");
